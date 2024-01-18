1142840a337d16fba9554bbaea75d8bc02ef99b5
Boyan <104321637+BoyanDashev@users.noreply.github.com>
2024-01-18 08:07:41 +0200

    Initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,12 @@ const About = () => {
   return (
     <section>
       <h2 className='text-2xl text-white font-bold mb-4'>About me.</h2>
-          <p className="text-white">I Started Programming in spring 2022. The first course that I took was the CS50x from Harvard. Then I started learning from the original documentations from various libraries like React, Three.js. Updating regulary myGithub.
-              And also many courses from FreecodeCamp.  </p>
-         <p className="text-white">Some of my skills:</p> 
+      <p className="text-white">I Started Programming in spring 2022.
+        The first course that I took was the CS50x from Harvard. Then I continued with variety of courses from Freecodecamp.
+        Then I started creating my own projects and learning from the original documentations
+        from variety of libraries like React, Tailwind.css, Three.js etc. 
+        </p>
+         <p className="text-white">Languages that I know.</p> 
           <ul className="text-white">
               <li>English: fluently</li>
               <li>German: level B1</li>
